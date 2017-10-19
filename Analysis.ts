di-unipi-socc/MProt

commit 538e55ebb44bcadae654497c44945ee8b68f398f

Some moderinzation and major perf improvements

Analysis.ts
@@ -36,30 +36,30 @@ module Analysis {
             public stateId: string) {
 
             // Check input states
-            for (var sid in states) {
-                var state = states[sid];
-                for (var c in state.caps)
+            for (const sid in states) {
+                const state = states[sid];
+                for (const c in state.caps)
                     if (!caps[c])
                         throw "Unknown capability " + c + " in state " + sid;
 
-                for (var r in state.reqs)
+                for (const r in state.reqs)
                     if (!reqs[r])
                         throw "Unknown requirement " + r + " in state " + sid;
 
-                for (var o in state.ops) {
+                for (const o in state.ops) {
                     if (!ops[o])
                         throw "Unknown operation " + o + " in state " + sid;
 
                     if (!state.ops[o].to)
                         throw "Unknown destination state " + state.ops[o].to + " in transition " + o + " from state " + sid;
 
-                    for (var i = 0; i < state.ops[o].reqs.length; i++)
-                        for (var r in state.ops[o].reqs[i])
+                    for (let i = 0; i < state.ops[o].reqs.length; i++)
+                        for (const r in state.ops[o].reqs[i])
                             if (!reqs[r])
                                 throw "Unknown requirement " + r + " in transition " + o + " from state " + sid;
                 }
 
-                for (var r in state.handlers) {
+                for (const r in state.handlers) {
                     if (!reqs[r])
                         throw "Unknown requirement " + r + " in fault handler from state " + sid;
                     if (!states[state.handlers[r]])
@@ -127,23 +127,23 @@ module Analysis {
                     public binding: Utils.Map<string>,
                     public containedBy: Utils.Map<string>,
                     public hasHardReset) {
-            var states = [];
-            for (var nodeId in nodes) {
-                var node = nodes[nodeId];
-                var nodeState = node.state;
+            const states = [];
+            for (const nodeId in nodes) {
+                const node = nodes[nodeId];
+                const nodeState = node.state;
                 states.push(nodeId + "=" + node.stateId);
                 this.reqs = Utils.setUnion(this.reqs, nodeState.reqs);
                 this.caps = Utils.setUnion(this.caps, nodeState.caps);
                 if (nodeState.isAlive && (nodeId in containedBy))
                     this.isContainmentConsistent = this.isContainmentConsistent && nodes[containedBy[nodeId]].state.isAlive;
 
-                for (var r in node.reqs)
+                for (const r in node.reqs)
                     this.reqNodeId[r] = nodeId;
-                for (var c in node.caps)
+                for (const c in node.caps)
                     this.capNodeId[c] = nodeId;
             }
 
-            for (var req in this.reqs)
+            for (const req in this.reqs)
                 if (!this.isReqSatisfied(req))
                     this.faults[req] = true;
 
@@ -156,7 +156,7 @@ module Analysis {
         }
 
         private areReqsSatisfied(reqs: Utils.Set) {
-            for (var req in reqs)
+            for (const req in reqs)
                 if (!this.isReqSatisfied(req))
                     return false;
 
@@ -176,8 +176,8 @@ module Analysis {
             if (!(opId in this.nodes[nodeId].state.ops))
                 return "The " + opId + " operation is not available in the current state of the " + nodeId + " node";
 
-            var opReqSets = this.nodes[nodeId].state.ops[opId].reqs;
-            for (var i = 0; i < opReqSets.length; i++)
+            const opReqSets = this.nodes[nodeId].state.ops[opId].reqs;
+            for (let i = 0; i < opReqSets.length; i++)
                 if (this.areReqsSatisfied(opReqSets[i]))
                     return "";
 
@@ -189,12 +189,12 @@ module Analysis {
         }
 
         performOp(nodeId: string, opId: string) {
-            var constraints = this.unsatisfiedOpConstraints(nodeId, opId);
+            const constraints = this.unsatisfiedOpConstraints(nodeId, opId);
             if (constraints)
                 throw constraints;
 
-            var nodes = Utils.cloneMap(this.nodes);
-            var node = nodes[nodeId];
+            const nodes = Utils.cloneMap(this.nodes);
+            const node = nodes[nodeId];
             nodes[nodeId] = node.performOp(opId);
             return new Application(nodes, this.binding, this.containedBy, this.hasHardReset);
         }
@@ -215,12 +215,12 @@ module Analysis {
         }
 
         handleFault(nodeId: string, r: string) {
-            var constraints = this.unsatisfiedHandlerConstraints(nodeId, r);
+            const constraints = this.unsatisfiedHandlerConstraints(nodeId, r);
             if (constraints)
                 throw constraints;
 
-            var nodes = Utils.cloneMap(this.nodes);
-            var node = nodes[nodeId];
+            const nodes = Utils.cloneMap(this.nodes);
+            const node = nodes[nodeId];
             nodes[nodeId] = node.handleFault(r);
             return new Application(nodes, this.binding, this.containedBy, this.hasHardReset);
         }
@@ -232,7 +232,7 @@ module Analysis {
             if (!(nodeId in this.containedBy))
                 return "The node " + nodeId + "is not contained in another node";
 
-            var container = this.containedBy[nodeId];
+            const container = this.containedBy[nodeId];
             if (this.nodes[container].state.isAlive)
                 return container + " (the container of " + nodeId + ") is alive";
         }
@@ -242,36 +242,36 @@ module Analysis {
         }
 
         doHardReset(nodeId: string) {
-            var constraints = this.unsatisfiedHardResetConstraints(nodeId);
+            const constraints = this.unsatisfiedHardResetConstraints(nodeId);
             if (constraints)
                 throw constraints;
 
-            var nodes = Utils.cloneMap(this.nodes);
-            var node = nodes[nodeId];
+            const nodes = Utils.cloneMap(this.nodes);
+            const node = nodes[nodeId];
             nodes[nodeId] = node.doHardReset();
             return new Application(nodes, this.binding, this.containedBy, this.hasHardReset);
         }
     }
 
     export function reachable(application: Application) {
-        var visited: Utils.Map<Application> = {};
-        var visit = function(app: Application) {
+        const visited: Utils.Map<Application> = {};
+        const visit = function(app: Application) {
             if (app.globalState in visited)
                 return;
 
             visited[app.globalState] = app;
 
-            for (var nodeId in app.nodes)
-                for (var opId in app.nodes[nodeId].ops)
+            for (const nodeId in app.nodes)
+                for (const opId in app.nodes[nodeId].ops)
                     if (app.canPerformOp(nodeId, opId))
                         visit(app.performOp(nodeId, opId));
 
-            for (var nodeId in app.nodes)
-                for (var req in app.nodes[nodeId].reqs)
+            for (const nodeId in app.nodes)
+                for (const req in app.nodes[nodeId].reqs)
                     if (app.canHandleFault(nodeId, req))
                         visit(app.handleFault(nodeId, req));
 
-            for (var nodeId in app.nodes)
+            for (const nodeId in app.nodes)
                 if (app.canHardReset(nodeId))
                     visit(app.doHardReset(nodeId));
         };
@@ -280,23 +280,24 @@ module Analysis {
     }
 
     export function plans(app: Application) {
-        var states = reachable(app);
-        var costs: Utils.Map<Utils.Map<number>> = {};
-        var steps: Utils.Map<Utils.Map<Step>> = {};
-
-        for (var s in states) {
-            costs[s] = { [s] : 0 };
-            steps[s] = {};
+        const stateData = reachable(app);
+        const states = Object.keys(stateData);
+        const stateIdx = {};
+        for (let i = 0; i < states.length; i++) {
+            stateIdx[states[i]] = i;
         }
 
-        for (var src in states) {
-            var state = states[src];
+        const costs = states.map((_, i) => states.map((_, j) => i === j ? 0 : NaN));
+        const steps: Step[][] = states.map((_, i) => states.map((_, j) => undefined));
 
-            for (var nodeId in state.nodes)
-                for (var opId in state.nodes[nodeId].ops)
+        for (let src = 0; src < states.length; src++) {
+            const state = stateData[states[src]];
+
+            for (const nodeId in state.nodes)
+                for (const opId in state.nodes[nodeId].ops)
                     if (state.canPerformOp(nodeId, opId)) {
-                        var dst = state.performOp(nodeId, opId).globalState;
-                        var newCost = 1; // TODO: we might want to compute the cost in a clever way
+                        const dst = stateIdx[state.performOp(nodeId, opId).globalState];
+                        const newCost = 1; // TODO: we might want to compute the cost in a clever way
                         // ! used to abuse NaN comparison (which always compares as false)
                         if (!(costs[src][dst] <= newCost)) {
                             costs[src][dst] = newCost;
@@ -304,11 +305,11 @@ module Analysis {
                         }
                     }
 
-            for (var nodeId in state.nodes)
-                for (var req in state.nodes[nodeId].reqs)
+            for (const nodeId in state.nodes)
+                for (const req in state.nodes[nodeId].reqs)
                     if (state.canHandleFault(nodeId, req)) {
-                        var dst = state.handleFault(nodeId, req).globalState;
-                        var newCost = 1; // TODO: we might want to compute the cost in a clever way
+                        const dst = stateIdx[state.handleFault(nodeId, req).globalState];
+                        const newCost = 1; // TODO: we might want to compute the cost in a clever way
                         // ! used to abuse NaN comparison (which always compares as false)
                         if (!(costs[src][dst] <= newCost)) {
                             costs[src][dst] = newCost;
@@ -316,10 +317,10 @@ module Analysis {
                         }
                     }
 
-            for (var nodeId in state.nodes)
+            for (const nodeId in state.nodes)
                 if (state.canHardReset(nodeId)) {
-                    var dst = state.doHardReset(nodeId).globalState;
-                    var newCost = 1; // TODO: we might want to compute the cost in a clever way
+                    const dst = stateIdx[state.doHardReset(nodeId).globalState];
+                    const newCost = 1; // TODO: we might want to compute the cost in a clever way
                     // ! used to abuse NaN comparison (which always compares as false)
                     if (!(costs[src][dst] <= newCost)) {
                         costs[src][dst] = newCost;
@@ -328,12 +329,12 @@ module Analysis {
                 }
         }
 
-        for (var via in states) {
-            for (var src in states) {
-                if (src == via || !(via in costs[src]))
+        for (let via = 0; via < states.length; via++) {
+            for (let src = 0; src < states.length; src++) {
+                if (src == via || isNaN(costs[src][via]))
                     continue;
 
-                for (var dst in costs[via]) {
+                for (let dst = 0; dst < states.length; dst++) {
                     // ! used to abuse NaN comparison (which always compares as false)
                     if (!(costs[src][dst] <= costs[src][via] + costs[via][dst])) {
                         costs[src][dst] = costs[src][via] + costs[via][dst];
@@ -343,6 +344,23 @@ module Analysis {
             }
         }
 
-        return { costs: costs, steps: steps };
+        const costsMap = {};
+        const stepsMap = {};
+        for (let src = 0; src < states.length; src++) {
+            const costSrcMap = {};
+            const stepSrcMap = {};
+            for (let dst = 0; dst < states.length; dst++) {
+                if (!isNaN(costs[src][dst])) {
+                    const dstString = states[dst];
+                    costSrcMap[dstString] = costs[src][dst];
+                    stepSrcMap[dstString] = steps[src][dst];
+                }
+            }
+            const srcString = states[src];
+            costsMap[srcString] = costSrcMap;
+            stepsMap[srcString] = stepSrcMap;
+        }
+
+        return { costs: costsMap, steps: stepsMap };
     }
 }

commit f203ce7f91e3be5904e4a7124fb2747f7175d477

Fix bug introduced in 538e55ebb44bcadae654497c44945ee8b68f398f
Never update a distance to NaN.

Analysis.ts
@@ -336,8 +336,9 @@ module Analysis {
 
                 for (let dst = 0; dst < states.length; dst++) {
                     // ! used to abuse NaN comparison (which always compares as false)
-                    if (!(costs[src][dst] <= costs[src][via] + costs[via][dst])) {
-                        costs[src][dst] = costs[src][via] + costs[via][dst];
+                    const newCost = costs[src][via] + costs[via][dst];
+                    if (!isNaN(newCost) && !(costs[src][dst] <= newCost)) {
+                        costs[src][dst] = newCost;
                         steps[src][dst] = steps[src][via];
                     }
                 }